0149e885344f16429ead3b8c30bdeaa7efcf8618
Tanay-codestar <sowbrmp@gmail.com>
2024-12-15 17:17:17 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#average
+math=100
+technology=99.99
+literacy=77.89999
+science=99.765
+health=93.123456789
+average=(math+technology+literacy+science+health)//5
+print("average:",average)
+percentage=((math+technology+literacy+ science+health)/500)*100
+print("percentage:",percentage)
+print(9**0.5)
